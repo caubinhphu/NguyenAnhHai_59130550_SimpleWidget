2d4eb9ee1f6a8968047f98f5268f8b8b134116d3
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-04-27 06:44:16 +0700

    create Person class

--- app/src/main/java/vn/edu/ntu/nguyenanhhai/nguyenanhhai_59130550_simplewidget/Person.java
+++ app/src/main/java/vn/edu/ntu/nguyenanhhai/nguyenanhhai_59130550_simplewidget/Person.java
@@ -1,5 +1,6 @@
 package vn.edu.ntu.nguyenanhhai.nguyenanhhai_59130550_simplewidget;
 
+import android.text.TextUtils;
 import java.util.ArrayList;
 
 public class Person {
@@ -8,5 +9,34 @@ public class Person {
   private String gender;
   private ArrayList<String> hobbies;
 
-  
+  // setter
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setBirthDate(String birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public void setHobbies(ArrayList<String> hobbies) {
+    this.hobbies = hobbies;
+  }
+
+  // add a hobby to the hobbies
+  public void addHobby(String hobby) {
+    hobbies.add(hobby);
+  }
+
+  // toString Person
+  @Override
+  public String toString() {
+    return name + '\n' +
+            "Ngày sinh: " + birthDate + '\n' +
+            "Giới tính: " + gender + '\n' +
+            "Sở thích: " + TextUtils.join("; ", hobbies);
+  }
 }
